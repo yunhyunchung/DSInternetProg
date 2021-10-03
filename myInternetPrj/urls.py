d7470af6e3a7e7b91fa7a0adef0d6fbf3b9f9a7f
yunhyunchung <yhchungsera@gmail.com>
2021-10-03 17:02:23 +0900

    Post 모델에 이미지와 파일 필드 추가하기

--- myInternetPrj/urls.py
+++ myInternetPrj/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('blog/', include('blog.urls')),  # 서버IP/blog
     path('admin/', admin.site.urls),      # 서버IP/admin
     path('', include('single_pages.urls')),   #서버IP/
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  #서버IP/media
